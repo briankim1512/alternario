f433ea9378a1687f6ecc014312c16045dec83d08
briankim1512@gmail.com <briankim1512@gmail.com>
2020-06-22 03:35:24 +0900

    Build game panel and populate general chat

--- app/src/static/js/game.js
+++ app/src/static/js/game.js
@@ -1,3 +1,16 @@
+let generalChat = document.getElementById('general-chat')
+let generalChatStatus = false
+
 document.getElementById('settings-cog').addEventListener('click', function () {
     settingsLanding.style.display = 'block'
+})
+
+document.getElementById('general-chat-bar').addEventListener('click', function () {
+    if (generalChatStatus) {
+        generalChat.style.height = '50px'
+        generalChatStatus = false
+    } else {
+        generalChat.style.height = '400px'
+        generalChatStatus = true
+    }
 })
